aa15e3c36dce3ff970641ad91777e3df1668cabb
MLMridul <mdmridul344@gmail.com>
2021-10-07 22:07:27 +0600

    Code Add

--- js/custom.js
+++ js/custom.js
@@ -1,2 +1,12 @@
-
-
+$(document).ready(function(){
+    var element = $('#countdown-gampang');
+    var finish_d = new Date();
+finish_d.setDate(finish_d.getDate() + 1);
+    element.CountdownGampang({
+        rampung: finish_d,
+        theme: "flat-colors-very-wide"
+    }, function(){
+        // callback that is fired when the countdown reaches 0.
+        alert("Wes Wayahe Bro :)");
+    });
+});
