6b209c16e49b23848efa62ebc16ed943972aef0a
MLMridul <mdmridul344@gmail.com>
2021-10-12 01:52:28 +0600

    Code Add

--- js/custom.js
+++ js/custom.js
@@ -9,4 +9,44 @@ finish_d.setDate(finish_d.getDate() + 1);
         // callback that is fired when the countdown reaches 0.
         alert("Wes Wayahe Bro :)");
     });
+
+
+    // Preloader
+
+
+    $('.preloader').delay(1000).slideUp(2000);
+    $('.loader').fadeOut(1000);
+
+     // ScroolTop
+
+     $(window).scroll(function () {
+        var scrolling = $(this).scrollTop();
+        if (scrolling > 400) {
+            $('.scrollTop').slideDown()
+        } else {
+            $('.scrollTop').slideUp()
+        }
+    })
+
+    $('.scrollTop').click(function () {
+        $('body ,html').animate({
+            scrollTop: 0
+        }, 2000)
+    })
+
+    //animation scroll js
+    var html_body = $('html, body');
+    $('nav a').on('click', function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                html_body.animate({
+                    scrollTop: target.offset().top - 0
+                }, 1500, );
+                return false;
+            }
+        }
+    });
+
 });
